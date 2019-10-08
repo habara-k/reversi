c926e42aba470d885f6113c2f11cd3d053f00158
Keigo Habara <k.habara.aa@gmail.com>
2019-10-08 12:24:10 +0900

    Remove Interface: IPlayerHelper

--- playerhelper/player_helper.py
+++ playerhelper/player_helper.py
@@ -2,15 +2,7 @@ from board.board import IBoard
 from position.position import Position
 
 
-class IPlayerHelper:
-    def select(self, board: IBoard) -> Position:
-        raise NotImplementedError()
-
-    def swap(self):
-        raise NotImplementedError()
-
-
-class PlayerHelper(IPlayerHelper):
+class PlayerHelper:
     def select(self, board: IBoard) -> Position:
         return Position()
 
